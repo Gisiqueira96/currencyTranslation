9e187d8b81019ab4af3456c1a035c0b014a36683
Gisiqueira96 <giovanna.penedos@outlook.com>
2021-10-09 17:16:49 -0300

    Adicionar Spring - Commit Inicial

---
+++ src/main/java/com/currencytranslation/currencyTranslation/CurrencyTranslationApplication.java
@@ -0,0 +1,13 @@
+package com.currencytranslation.currencyTranslation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyTranslationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyTranslationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currencytranslation/currencyTranslation/CurrencyTranslationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencytranslation.currencyTranslation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyTranslationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
